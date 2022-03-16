c6525afbec6ccd242ab82287a8eb5d75ab36f5e7
csyed <smachisty92@gmail.com>
2022-03-16 12:34:45 +0530

    terraform

--- seed.groovy
+++ seed.groovy
@@ -1,13 +1,47 @@
-freeStyleJob('example') {
+folder('Terraform') {
+    displayName('Terraform')
+    description('Terraform')
+}
+
+freeStyleJob('Terraform-VPC') {
     scm {
         git {
             remote {
                 name('origin')
-                url('https://github.com/smachisty92/ansible.git')
+                url('https://github.com/smachisty92/terraform-vpc.git')
             }
         }
     }
     steps {
-        shell('ls -ltr')
+        shell('make dev')
     }
 }
+
+freeStyleJob('terraform-mutable-alb') {
+    scm {
+        git {
+            remote {
+                name('origin')
+                url('https://github.com/smachisty92/terraform-mutable-alb.git')
+            }
+        }
+    }
+    steps {
+        shell('make')
+    }
+}
+
+freeStyleJob('terraform-databases') {
+    scm {
+        git {
+            remote {
+                name('origin')
+                url('https://github.com/smachisty92/terraform-databases.git')
+            }
+        }
+    }
+    steps {
+        shell('make')
+    }
+}
+
